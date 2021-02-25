6cadbefa451f67da62b8f463e5be0e9ba94c6931
OroArmor <eliorona@live.com>
2021-02-25 15:30:51 -0800

    Update for new versions of minecraft, but it breaks somewhere

--- src/main/java/com/oroarmor/block_quantity_scanner/BlockQuantityScanner.java
+++ src/main/java/com/oroarmor/block_quantity_scanner/BlockQuantityScanner.java
@@ -1,31 +1,73 @@
 package com.oroarmor.block_quantity_scanner;
 
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 
+import net.minecraft.MinecraftVersion;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.command.argument.BlockStateArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class BlockQuantityScanner implements ModInitializer {
+
+    public static Block[] STONE_BLOCKS = addBlocksToArray("stone", "deepslate", "grimstone", "andesite", "granite", "diorite", "air");
+
+    public static Block[] DIAMOND_ORES = addBlocksToArray("DIAMOND_ORE", "DEEPSLATE_DIAMOND_ORE");
+    public static Block[] REDSTONE_ORES = addBlocksToArray("REDSTONE_ORE", "DEEPSLATE_REDSTONE_ORE");
+    public static Block[] GOLD_ORES = addBlocksToArray("GOLD_ORE", "DEEPSLATE_GOLD_ORE");
+    public static Block[] IRON_ORES = addBlocksToArray("IRON_ORE", "DEEPSLATE_IRON_ORE");
+    public static Block[] LAPIS_ORES = addBlocksToArray("LAPIS_ORE", "DEEPSLATE_LAPIS_ORE");
+    public static Block[] COAL_ORES = addBlocksToArray("COAL_ORE");
+    public static Block[] COPPER_ORES = addBlocksToArray("COPPER_ORE");
+    public static Block[] EMERALD_ORES = addBlocksToArray("EMERALD_ORE");
+    public static final Block[][] ORES = {DIAMOND_ORES, REDSTONE_ORES, GOLD_ORES, IRON_ORES, LAPIS_ORES, COAL_ORES, COPPER_ORES, EMERALD_ORES};
+    public static final String[] ORE_NAMES = {"diamond", "redstone", "gold", "lapis", "iron", "copper", "coal", "emerald"};
+
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register(BlockQuantityScanner::registerCommand);
+            // /execute positioned 0 100 0 run scan_all_ores 2048 140
+    }
+
+    private static Block[] addBlocksToArray(String... blocks) {
+        List<Block> blocksList = new ArrayList<>(blocks.length);
+        Block defaultBlock = Registry.BLOCK.get(Registry.BLOCK.getDefaultId());
+        for (String blockName : blocks) {
+            Block block = Registry.BLOCK.get(new Identifier(blockName.toLowerCase()));
+            if (block != defaultBlock) {
+                blocksList.add(block);
+            }
+        }
+        Object[] objects = blocksList.toArray();
+        Block[] array = new Block[objects.length];
+        for (int i = 0; i < objects.length; i++) {
+            array[i] = (Block) objects[i];
+        }
+        return array;
     }
 
     private static void registerCommand(CommandDispatcher<ServerCommandSource> commandSourceCommandDispatcher, boolean dedicated) {
@@ -37,7 +79,7 @@ public class BlockQuantityScanner implements ModInitializer {
                                     BlockState state = BlockStateArgumentType.getBlockState(context, "block").getBlockState();
                                     int range = IntegerArgumentType.getInteger(context, "range");
 
-                                    return scanBlocks(context, new BlockState[]{state}, range, -1);
+                                    return scanBlocks(context, state, range, -1);
                                 })
                                 .then(CommandManager.argument("max-height", IntegerArgumentType.integer())
                                         .executes(context -> {
@@ -45,7 +87,7 @@ public class BlockQuantityScanner implements ModInitializer {
                                             int range = IntegerArgumentType.getInteger(context, "range");
                                             int maxHeight = IntegerArgumentType.getInteger(context, "max-height");
 
-                                            return scanBlocks(context, new BlockState[]{state}, range, maxHeight);
+                                            return scanBlocks(context, state, range, maxHeight);
                                         }))));
 
         LiteralArgumentBuilder<ServerCommandSource> allOresCommand = LiteralArgumentBuilder.<ServerCommandSource>literal("scan_all_ores")
@@ -54,75 +96,214 @@ public class BlockQuantityScanner implements ModInitializer {
                         .executes(context -> {
                             int range = IntegerArgumentType.getInteger(context, "range");
 
-                            return scanBlocks(context, new BlockState[]{Blocks.DIAMOND_ORE.getDefaultState(), Blocks.GOLD_ORE.getDefaultState(), Blocks.IRON_ORE.getDefaultState(), Blocks.REDSTONE_ORE.getDefaultState(), Blocks.EMERALD_ORE.getDefaultState(), Blocks.COAL_ORE.getDefaultState(), Blocks.COPPER_ORE.getDefaultState(), Blocks.LAPIS_ORE.getDefaultState()},
-                                    range, -1);
-                        }));
+                            return scanOres(context, range, -1);
+                        }).then(CommandManager.argument("max-height", IntegerArgumentType.integer())
+                                .executes(context -> {
+                                    int range = IntegerArgumentType.getInteger(context, "range");
+                                    int maxHeight = IntegerArgumentType.getInteger(context, "max-height");
+
+                                    return scanOres(context, range, maxHeight);
+                                })));
 
         commandSourceCommandDispatcher.register(scanBlockCommand);
         commandSourceCommandDispatcher.register(allOresCommand);
     }
 
-    private static int scanBlocks(CommandContext<ServerCommandSource> context, BlockState[] states, int range, int maxHeight) {
+    private static int scanBlocks(CommandContext<ServerCommandSource> context, BlockState state, int range, int maxHeight) {
         ServerCommandSource source = context.getSource();
         ServerWorld world = source.getWorld();
 
-        BlockPos executionLocation = new BlockPos(source.getPosition());
-        int halfRange = range >> 1;
+        source.sendFeedback(new LiteralText("Starting Scan. This may take several minutes"), true);
+        Runnable scanBlocksRunnable = () -> {
+            BlockPos executionLocation = new BlockPos(source.getPosition());
+            int halfRange = range >> 1;
 
-        int endY = maxHeight == -1 ? world.getTopY() : maxHeight;
-        int bottomY = world.getBottomY();
-        int yRange = endY - bottomY;
+            int endY = maxHeight == -1 ? world.getTopY() : maxHeight;
+            int bottomY = world.getBottomY();
+            int yRange = endY - bottomY;
 
-        int[] levelsToCountsArr = new int[(endY - bottomY) * states.length];
+            int[] levelsToCountsArr = new int[(endY - bottomY)];
 
-        int cpuThreads = Runtime.getRuntime().availableProcessors();
-        Thread[] threads = new Thread[cpuThreads];
+            int cpuThreads = Runtime.getRuntime().availableProcessors();
+            Thread[] threads = new Thread[cpuThreads];
 
-        Consumer<BlockPos> positionConsumer = pos -> {
-            BlockState worldBlockState = world.getBlockState(pos);
-            int position = (pos.getY() - bottomY) * states.length;
-            for (int i = 0; i < states.length; i++) {
-                if (worldBlockState.isOf(states[i].getBlock())) {
-                    levelsToCountsArr[position + i]++;
+            Block block = state.getBlock();
+
+            Consumer<BlockPos> positionConsumer = pos -> {
+                BlockState worldBlockState = world.getBlockState(pos);
+                int position = (pos.getY() - bottomY);
+                if (worldBlockState.isOf(block)) {
+                    levelsToCountsArr[position]++;
                 }
-            }
-        };
+                world.setChunkForced(pos.getX() / 16, pos.getZ() / 16, false);
+            };
 
-        for (int i = 0; i < cpuThreads; i++) {
-            int finalI = i;
-            Runnable runnable = () -> {
-                for (int y = 0; y < yRange / cpuThreads; y++) {
+            for (int i = 0; i < cpuThreads; i++) {
+                int finalI = i;
+                Runnable runnable = () -> {
                     for (int x = -halfRange; x < halfRange; x++) {
                         for (int z = -halfRange; z < halfRange; z++) {
-                            positionConsumer.accept(new BlockPos(x + executionLocation.getX(), y + yRange / cpuThreads * finalI + bottomY, z + executionLocation.getZ()));
+                            for (int y = 0; y < yRange / cpuThreads; y++) {
+                                positionConsumer.accept(new BlockPos(x + executionLocation.getX(), y + yRange / cpuThreads * finalI + bottomY, z + executionLocation.getZ()));
+                            }
+                        }
+                    }
+                    System.out.printf("Thread %d has finished\n", finalI);
+                };
+                threads[i] = new Thread(runnable);
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String pattern = "HH-mm-ss-SSS";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            String date = simpleDateFormat.format(new Date());
+            String fileName = String.format("scan-%s-%s-location-%d-%d-range-%d-%s.csv", MinecraftVersion.GAME_VERSION.getName(), date, executionLocation.getX(), executionLocation.getZ(), range, Registry.BLOCK.getId(block).toString().replace(':', '-'));
+
+            File outputFolder = FabricLoader.getInstance().getGameDir().resolve("block-quantity-scanner").toFile();
+
+            outputFolder.mkdirs();
+
+            File output = outputFolder.toPath().resolve(fileName).toFile();
+            try {
+                output.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try (FileOutputStream stream = new FileOutputStream(output)) {
+                PrintStream printStream = new PrintStream(stream);
+                printStream.print("y");
+                printStream.print(", " + Registry.BLOCK.getId(block));
+                printStream.println();
+
+                for (int y = bottomY; y < endY; y++) {
+                    printStream.print(y);
+                    printStream.print(", " + levelsToCountsArr[(y - bottomY)]);
+                    printStream.println();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            source.sendFeedback(new LiteralText("Scan saved to " + fileName), true);
+        };
+
+        Thread scanBlocksThread = new Thread(scanBlocksRunnable);
+        scanBlocksThread.start();
+
+        return 1;
+    }
+
+    private static int scanOres(CommandContext<ServerCommandSource> context, int range, int maxHeight) {
+        ServerCommandSource source = context.getSource();
+        ServerWorld world = source.getWorld();
+
+        source.sendFeedback(new LiteralText("Starting Scan. This may take several minutes"), true);
+        Runnable scanBlocksRunnable = () -> {
+            BlockPos executionLocation = new BlockPos(source.getPosition());
+            int halfRange = range >> 1;
+
+            int endY = maxHeight == -1 ? world.getTopY() : maxHeight;
+            int bottomY = world.getBottomY();
+            int yRange = endY - bottomY;
+
+            int[] levelsToCountsArr = new int[(endY - bottomY) * ORES.length];
+
+            int cpuThreads = Runtime.getRuntime().availableProcessors();
+            Thread[] threads = new Thread[cpuThreads];
+
+            Consumer<BlockPos> positionConsumer = pos -> {
+                BlockState worldBlockState = world.getBlockState(pos);
+                Block worldBlock = worldBlockState.getBlock();
+                for (Block block : STONE_BLOCKS) {
+                    if (worldBlock == block)
+                        return;
+                }
+
+                int position = (pos.getY() - bottomY) * ORES.length;
+                for (int i = 0; i < ORES.length; i++) {
+                    for (Block block : ORES[i]) {
+                        if (worldBlock == block) {
+                            levelsToCountsArr[position + i]++;
                         }
                     }
                 }
             };
-            threads[i] = new Thread(runnable);
-            threads[i].start();
-        }
 
-        for (Thread thread : threads) {
+            for (int i = 0; i < cpuThreads; i++) {
+                int finalI = i;
+                Runnable runnable = () -> {
+                    for (int x = -halfRange; x < halfRange; x++) {
+                        for (int z = -halfRange; z < halfRange; z++) {
+                            for (int y = 0; y < yRange / cpuThreads; y++) {
+                                positionConsumer.accept(new BlockPos(x + executionLocation.getX(), y + yRange / cpuThreads * finalI + bottomY, z + executionLocation.getZ()));
+                            }
+                        }
+                    }
+                    System.out.printf("Thread %d has finished\n", finalI);
+                };
+                threads[i] = new Thread(runnable);
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String pattern = "HH-mm-ss-SSS";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            String date = simpleDateFormat.format(new Date());
+            String fileName = String.format("scan-%s-%s-location-%d-%d-range-%d-ores.csv", MinecraftVersion.GAME_VERSION.getName(), date, executionLocation.getX(), executionLocation.getZ(), range);
+
+            File outputFolder = FabricLoader.getInstance().getGameDir().resolve("block-quantity-scanner").toFile();
+
+            outputFolder.mkdirs();
+
+            File output = outputFolder.toPath().resolve(fileName).toFile();
             try {
-                thread.join();
-            } catch (InterruptedException e) {
+                output.createNewFile();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+            try (FileOutputStream stream = new FileOutputStream(output)) {
+                PrintStream printStream = new PrintStream(stream);
+                printStream.print("y");
+                for (String name : ORE_NAMES) {
+                    printStream.print(", " + name);
+                }
+                printStream.println();
 
-        System.out.print("y");
-        for (BlockState state : states) {
-            System.out.print(", " + Registry.BLOCK.getId(state.getBlock()));
-        }
-        System.out.println();
-
-        for (int y = bottomY; y < endY; y++) {
-            System.out.print(y);
-            for (int i = 0; i < states.length; i++) {
-                System.out.print(", " + levelsToCountsArr[(y - bottomY) * states.length + i]);
+                for (int y = bottomY; y < endY; y++) {
+                    printStream.print(y);
+                    for (int i = 0; i < ORES.length; i++) {
+                        printStream.print(", " + levelsToCountsArr[(y - bottomY) * ORES.length + i]);
+                    }
+                    printStream.println();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println();
+
+            source.sendFeedback(new LiteralText("Scan saved to " + fileName), true);
+        };
+
+        Thread scanBlocksThread = new Thread(scanBlocksRunnable);
+        scanBlocksThread.start();
+        try {
+            scanBlocksThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         return 1;
